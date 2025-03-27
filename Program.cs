8b9b02580343cd7db07f65527869f7362c8b6688
Ana Hoeft <ahoeft7@my.wctc.edu>
2025-03-27 13:59:57 -0500

    [1.19d] List the character(s) created in that 1981 (Mario series) - return character name only.

--- Program.cs
+++ Program.cs
@@ -116,4 +116,9 @@ Console.Clear();
 int charactercount = characters.Count(c => c.YearCreated == 1981 && c.Series.Contains("Mario"));
 Console.WriteLine($"There are {charactercount} characters created in 1981 in the Mario series");
 
+//List the character(s) created in 1981 (Mario series) return character name only
+foreach(Character character in characters.Where(c => c.YearCreated == 1981 && c.Series.Contains("Mario")))
+{
+  Console.WriteLine($"Name: {character.Name}\n");
+}
 
--- obj/Debug/net8.0/LINQdemo.AssemblyInfo.cs
+++ obj/Debug/net8.0/LINQdemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LINQdemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be11046fc9b594e572e7c937e796781ef090ea3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fef32ef0e3cc5d1bc17eb974792f3bea57cafc23")]
 [assembly: System.Reflection.AssemblyProductAttribute("LINQdemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LINQdemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
